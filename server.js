a17148e3c114c672db01432a25c731be5bd75eb5
siddheshhadkar <siddheshhadkar07@gmail.com>
2021-08-27 20:14:27 +0530

    Added script for heroku

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.get("/", (req, res) => {
